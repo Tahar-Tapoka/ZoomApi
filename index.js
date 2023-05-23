851635f93ac9ade8733c5de0607549df316190fd
tahar <taharboussak@gmail.com>
2023-05-23 16:15:18 +0100

    1st call

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+const port = 3001;
+const users = [];
+
+app.get("/", (req, res) => {
+  res.send("Hello there");
+});
+
+const addUser = (user, roomId) => users.push({ user, roomId });
+
+const removeUser = (user) => {
+  users = users.filter((usr) => usr.user != user);
+};
+
+const getRoomUsers = (roomId) => users.filter((usr) => usr.roomId == roomId);
+
+io.on("connection", (socket) => {
+  console.log("Someone just connected");
+  socket.on("join-room", ({ user, roomId }) => {
+    console.log(user, " joined room ", roomId);
+    socket.join(roomId);
+    addUser(user, roomId);
+    socket.to(roomId).emit("connected", user);
+    io.to(roomId).emit("room-users", getRoomUsers(roomId));
+
+    socket.on("disconnect", () => {
+      console.log("disconnected");
+      socket.leave(roomId);
+      removeUser(user);
+      io.to(roomId).emit("room-users", getRoomUsers(roomId));
+    });
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Zoom Clone Api listenning on localhost:3001`);
+});
